59b651244662fb31b5ca30dc574aac83148ce446
williamsongigi69 <50307317+williamsongigi69@users.noreply.github.com>
2019-10-12 19:48:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
-from flask import Flask, render_template, jsonify, redirect
-from flask_pymongo import PyMongo
+# import dependencies
+from flask import Flask, jsonify, render_template, request
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import pymongo
 import scrape_mars
 
+# FLASK SETUP. Create an app, being sure to pass __name__
 app = Flask(__name__)
 
-mongo = PyMongo(app)
+# Connect to MongoDB
+conn = "mongodb://localhost:27017"
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
 
-@app.route('/')
-def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+# Define database and collection. Will create one if not already available.
+db = client.mars_db
+collection = db.items
 
-@app.route('/scrape')
-def scrape():
-    mars = mongo.db.mars
-    mars_data = scrape_mars.scrape()
-    mars.update(
-        {},
-        mars_data,
-        upsert=True
-    )
-    return redirect('/', code=302)
+# Note:
+# Make sure MongoDB is running on your computer by typing `mongod` in a terminal window
+
+# Flask Routes
+
+@app.route("/")
+def home_page():
+    # Read the mongo db data
+    mars_data = collection.find_one()
+    # send it along with the html page under the home `/` route function
+    return render_template('index.html', payload=mars_data)
+
+@app.route("/scrape")
+def scrape_all():
+    scrape_dic = scrape_mars.scrape()
+    print('scrape_result:', scrape_dic)
+    #  Put the data into the mongo db from the scraping function under the `/scrape` route function
+    collection.update({}, scrape_dic, upsert=True)
+    return f'Scraping done!'
 
 if __name__ == "__main__":
     app.run(debug=True)
